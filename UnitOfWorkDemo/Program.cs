93fc4223100f1b718fe99eaf3a45f2997bab1ab1
Saif Abdullah <saif.alsaidi@mol.gov.om>
2022-11-24 13:15:15 +0400

    add

--- UnitOfWorkDemo/Program.cs
+++ UnitOfWorkDemo/Program.cs
@@ -7,9 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var PeopleContextConnection = builder.Configuration.GetConnectionString("PaymentHub") ?? throw new InvalidOperationException("Connection string 'PaymentHub' not found.");
-
-var DBSTGPassword = Environment.GetEnvironmentVariable("DBSTGPassword", EnvironmentVariableTarget.Machine).ToString();
-builder.Services.AddDbContext<PeopleContext>(options => options.UseOracle(PeopleContextConnection + DBSTGPassword).LogTo(Console.WriteLine));
+builder.Services.AddDbContext<PeopleContext>(options => options.UseOracle(PeopleContextConnection).LogTo(Console.WriteLine));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
